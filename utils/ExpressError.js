d4cbf0ae6c49e2b6e56b3364612d1424656091c4
Devadharshan <dharshandeva01@gmail.com>
2021-03-10 22:05:34 +0530

    initial not full

---
+++ utils/ExpressError.js
@@ -0,0 +1,9 @@
+class ExpressError extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = messgae;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ExpressError;
